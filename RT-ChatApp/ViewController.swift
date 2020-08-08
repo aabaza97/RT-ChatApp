c9d2151032220d918b7f5f8b355d1344ff71db41
Ahmed Abaza <aabaza32@hotmail.com>
2020-08-08 18:49:43 +0200

    Initial Commit

---
+++ RT-ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RT-ChatApp
+//
+//  Created by Ahmed Abaza on 8/8/20.
+//  Copyright © 2020 eyecorps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
