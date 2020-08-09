da622265ecc4531260449c4c2deca2094e8220c8
Ahmed Abaza <aabaza32@hotmail.com>
2020-08-09 05:41:02 +0200

    Handle Auth sessions, add DB Manager

---
+++ RT-ChatApp/Models/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  RT-ChatApp
+//
+//  Created by Ahmed Abaza on 8/8/20.
+//  Copyright © 2020 eyecorps. All rights reserved.
+//
+
+import Foundation
---
+++ RT-ChatApp/Resources/DBManager.swift
@@ -0,0 +1,9 @@
+//
+//  DBManager.swift
+//  RT-ChatApp
+//
+//  Created by Ahmed Abaza on 8/9/20.
+//  Copyright © 2020 eyecorps. All rights reserved.
+//
+
+import Foundation
